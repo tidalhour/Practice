7d85529f9bb2feb90f43af439e3fe3a13a9b036e
Beatriz Marie Bautista <beatrizmarie.bautista@gmail.com>
2024-10-24 21:37:48 +0800

    changed to md file from java file

--- 12. ON-BREAK.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-	public static void main(String[] args) 
-	{
-		System.out.println("No code for now, taking a break! I'm still trying to study for-loops, arrays and multi-dimensional arrays. It's quite the head-scratcher tbh.");
-    System.out.println("I also need to rest as I just came from an exhausting day.");
-	}
-}
