5fdd6b930a6bd73de9f8fea971a072f7f9caf7c9
Beatriz Marie Bautista <beatrizmarie.bautista@gmail.com>
2024-10-28 10:32:12 +0800

    Update

---
+++ 18. Methods.java
@@ -0,0 +1,13 @@
+public class File_class {
+	static void myMethod() {
+		System.out.println("Hello");
+	}
+public static void main(String[] args) {
+   for (int a=1; a<=5; a++){
+   	myMethod();
+   }
+ }
+/*
+* Simple method test, expected output will print Hello 5 times. Did this in school.
+*/
+}
