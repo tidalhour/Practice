35646b67fb00ac95da308db2cd24ac16647d6ec1
Beatriz Marie Bautista <beatrizmarie.bautista@gmail.com>
2025-03-19 16:02:40 +0800

    Create 35. triangle thingy.java

---
+++ 35. triangle thingy.java
@@ -0,0 +1,18 @@
+public class Funny {
+
+  //from this video: https://youtube.com/shorts/4FcgRPMH5G4?si=pTpujfkjW-xUpFjQ
+  //bro ever since i stopped doing daily coding things this TOOK me a while to think about and like bruh i couldve just done a for loop where a < b (a being the outer variable incremented and b being the inner value incremented) but like seriously though what is wrong with me and my brain
+  //alright lemme go back to learning about Jswing i still wanna make a GUI (hehe gooey)
+  //also hi i still haven't finished my part for the STF programming project my bad
+  // me when i make these practice codes my diary
+
+	public static void main(String[] args) {
+		
+		String aster = "*";
+		for (int i=0; i<5; i++)
+		{
+			System.out.println(aster);
+			aster = aster + "*";
+		}
+	}
+}
